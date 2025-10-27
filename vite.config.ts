f93766c68cab6a25b75f7bcd931a3d97a4efe5c3
LucasRgo <lucas.lrg0005@gmail.com>
2025-10-27 10:56:25 -0300

    UPdate

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    allowedHosts: ["beloved-barnacle-smart.ngrok-free.app"],
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/",
+    server: {
+        host: "::",
+        port: 8080,
+        allowedHosts: ["beloved-barnacle-smart.ngrok-free.app", "LucasRgo.github.io", "protocoloantiperda.com.br"],
     },
-  },
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
