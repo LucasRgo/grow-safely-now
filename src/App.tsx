ff66c7cc3201fdeba7fa9427d41d2c56c8aae63b
LucasRgo <lucas.lrg0005@gmail.com>
2025-11-04 16:23:40 -0300

    HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { CountdownProvider } from "@/components/context/CountdownContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -16,14 +16,14 @@ const App = () => (
             <CountdownProvider>
                 <Toaster />
                 <Sonner />
-                <BrowserRouter>
+                <HashRouter>
                     <Routes>
                         <Route path="/operacoes-milionarias" element={<OperacoesMilionarias />} />
                         <Route path="/" element={<Index />} />
                         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                         <Route path="*" element={<NotFound />} />
                     </Routes>
-                </BrowserRouter>
+                </HashRouter>
             </CountdownProvider>
         </TooltipProvider>
     </QueryClientProvider>
